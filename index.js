c7900bb023ff81d38f01a81a3c7d68876927d352
Khowshik Sunny <khowshik.sunny7@gmail.com>
2025-08-29 03:40:09 +0600

    heart count function done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// get innertext Number
+function getInnerTextNumber(id) {
+    return parseInt(document.getElementById(id).innerText)
+}
 
+// Get raw innerText
+function getInnerText(id) {
+    return document.getElementById(id).innerText
+}
+
+
+
+
+// heart icon click function
+
+const hearts = document.getElementsByClassName('card-heart');
+
+for (const heart of hearts) {
+    heart.addEventListener('click', function () {
+        let heartCount = getInnerTextNumber('nav-heart');
+        let newCount = heartCount + 1;
+
+        document.getElementById('nav-heart').innerText = newCount;
+    });
+}
